0161115b5c30668c0495d00e8a7f8e93cb18957b
Adamsmith6300 <adamsmith6300@gmail.com>
2025-08-24 18:02:10 -0700

    initial nodejs/nestjs server setup and containerized

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const port = process.env.PORT || 8080;
+  
+  app.enableCors();
+  
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+
+bootstrap();
